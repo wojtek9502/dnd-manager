b6eb1cec6612f186f44e3b98f6b8dc4c52f81959
wojtek9502 <wojtek.klusek@gmail.com>
2024-02-07 21:14:36 +0100

    add api healthz endpoint test

--- app/modules/api/auth.py
+++ app/modules/api/auth.py
@@ -17,9 +17,9 @@ def _is_token_valid_with_main_token(api_key) -> bool:
 
 
 async def validate_api_key(api_key: str = Security(api_key_header)):
-    token_service = UserTokenService()
+    user_token_service = UserTokenService()
 
-    is_token_valid = token_service.is_token_valid(token=api_key)
+    is_token_valid = user_token_service.is_token_valid(token=api_key)
     if not is_token_valid:
         is_token_valid = _is_token_valid_with_main_token(api_key=api_key)
 
